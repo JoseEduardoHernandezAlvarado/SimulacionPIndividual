c4ca65952f1025e945d962869eeaf391417b3d90
EDUARDO HERNANDEZ <lala.jose56@gmail.com>
2025-05-21 13:06:01 -0700

    Falta diseño

--- SimulacionPIndividual/Form1.Designer.cs
+++ SimulacionPIndividual/Form1.Designer.cs
@@ -28,13 +28,217 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.lblConclusion = new System.Windows.Forms.Label();
+            this.nudSimulaciones = new System.Windows.Forms.NumericUpDown();
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.nudClientes = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nudDespacho = new System.Windows.Forms.NumericUpDown();
+            this.dataGridViewResults = new System.Windows.Forms.DataGridView();
+            this.chartWaitingTimes = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.BtnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSimulaciones)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudClientes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDespacho)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResults)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartWaitingTimes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblConclusion
+            // 
+            this.lblConclusion.AutoSize = true;
+            this.lblConclusion.Location = new System.Drawing.Point(552, 66);
+            this.lblConclusion.MaximumSize = new System.Drawing.Size(500, 300);
+            this.lblConclusion.Name = "lblConclusion";
+            this.lblConclusion.Size = new System.Drawing.Size(35, 13);
+            this.lblConclusion.TabIndex = 1;
+            this.lblConclusion.Text = "label1";
+            // 
+            // nudSimulaciones
+            // 
+            this.nudSimulaciones.Location = new System.Drawing.Point(930, 409);
+            this.nudSimulaciones.Maximum = new decimal(new int[] {
+            40,
+            0,
+            0,
+            0});
+            this.nudSimulaciones.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nudSimulaciones.Name = "nudSimulaciones";
+            this.nudSimulaciones.Size = new System.Drawing.Size(120, 20);
+            this.nudSimulaciones.TabIndex = 2;
+            this.nudSimulaciones.Tag = "1";
+            this.nudSimulaciones.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.AllowDrop = true;
+            this.btnEjecutar.Location = new System.Drawing.Point(628, 328);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(129, 23);
+            this.btnEjecutar.TabIndex = 3;
+            this.btnEjecutar.Text = "Iniciar simulacion";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.Ejecutar_Click);
+            // 
+            // nudClientes
+            // 
+            this.nudClientes.Location = new System.Drawing.Point(930, 451);
+            this.nudClientes.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nudClientes.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudClientes.Name = "nudClientes";
+            this.nudClientes.Size = new System.Drawing.Size(120, 20);
+            this.nudClientes.TabIndex = 4;
+            this.nudClientes.Tag = "1";
+            this.nudClientes.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(832, 399);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 30);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Cantidad de simulaciones";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(832, 453);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 30);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Clientes cada 2 minutos";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(814, 501);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 30);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Tiempo de despacho por cliente(Minutos)";
+            // 
+            // nudDespacho
+            // 
+            this.nudDespacho.Location = new System.Drawing.Point(930, 501);
+            this.nudDespacho.Maximum = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            this.nudDespacho.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudDespacho.Name = "nudDespacho";
+            this.nudDespacho.Size = new System.Drawing.Size(120, 20);
+            this.nudDespacho.TabIndex = 9;
+            this.nudDespacho.Tag = "1";
+            this.nudDespacho.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // dataGridViewResults
+            // 
+            this.dataGridViewResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewResults.Location = new System.Drawing.Point(16, 372);
+            this.dataGridViewResults.Name = "dataGridViewResults";
+            this.dataGridViewResults.Size = new System.Drawing.Size(772, 242);
+            this.dataGridViewResults.TabIndex = 10;
+            // 
+            // chartWaitingTimes
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chartWaitingTimes.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chartWaitingTimes.Legends.Add(legend3);
+            this.chartWaitingTimes.Location = new System.Drawing.Point(16, 66);
+            this.chartWaitingTimes.Name = "chartWaitingTimes";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chartWaitingTimes.Series.Add(series3);
+            this.chartWaitingTimes.Size = new System.Drawing.Size(376, 300);
+            this.chartWaitingTimes.TabIndex = 11;
+            this.chartWaitingTimes.Text = "chart1";
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.AllowDrop = true;
+            this.BtnClear.Location = new System.Drawing.Point(817, 328);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(129, 23);
+            this.BtnClear.TabIndex = 12;
+            this.BtnClear.Text = "Limpiar datos";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1100, 635);
+            this.Controls.Add(this.BtnClear);
+            this.Controls.Add(this.chartWaitingTimes);
+            this.Controls.Add(this.dataGridViewResults);
+            this.Controls.Add(this.nudDespacho);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nudClientes);
+            this.Controls.Add(this.btnEjecutar);
+            this.Controls.Add(this.nudSimulaciones);
+            this.Controls.Add(this.lblConclusion);
+            this.Name = "Form1";
+            this.Text = "Simulador de cajas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudSimulaciones)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudClientes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDespacho)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResults)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartWaitingTimes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label lblConclusion;
+        private System.Windows.Forms.NumericUpDown nudSimulaciones;
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.NumericUpDown nudClientes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown nudDespacho;
+        private System.Windows.Forms.DataGridView dataGridViewResults;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartWaitingTimes;
+        private System.Windows.Forms.Button BtnClear;
     }
 }
 
--- SimulacionPIndividual/Form1.cs
+++ SimulacionPIndividual/Form1.cs
@@ -7,14 +7,330 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace SimulacionPIndividual
 {
     public partial class Form1 : Form
     {
+        // Parámetros de la simulación
+        private double LAMBDA;
+        private double MU;
+        private int SIMULATION_TIME = 120;
+        private int NUM_RUNS;
+        private List<double> averageWaitingTimesMM1 = new List<double>();
+        private List<double> averageServiceTimesMM1 = new List<double>();
+        private List<double> averageQueueLengthsMM1 = new List<double>();
+        private List<double> averageWaitingTimesMM2 = new List<double>();
+        private List<double> averageServiceTimesMM2 = new List<double>();
+        private List<double> averageQueueLengthsMM2 = new List<double>();
+
+        // Parámetros del generador de números pseudoaleatorios
+        private int a = 23; // Constante multiplicativa
+        private int c = 101; // Constante aditiva
+        private int m = 1009; // Módulo
+
         public Form1()
         {
             InitializeComponent();
+
+            // Configurar DataGridView
+            dataGridViewResults.Columns.Clear();
+            dataGridViewResults.Columns.Add("Corrida", "Corrida");
+            dataGridViewResults.Columns.Add("TiempoEspera", "Tiempo Espera (min)");
+            dataGridViewResults.Columns.Add("TiempoDespacho", "Tiempo Despacho (min)");
+            dataGridViewResults.Columns.Add("LongitudCola", "Longitud Cola");
+            dataGridViewResults.Columns.Add("Modelo", "Modelo");
+            dataGridViewResults.Columns["TiempoEspera"].DefaultCellStyle.Format = "N2";
+            dataGridViewResults.Columns["TiempoDespacho"].DefaultCellStyle.Format = "N2";
+            dataGridViewResults.Columns["LongitudCola"].DefaultCellStyle.Format = "N2";
+
+            // Configurar Chart
+            chartWaitingTimes.Series.Clear();
+            chartWaitingTimes.ChartAreas.Clear();
+            chartWaitingTimes.ChartAreas.Add(new ChartArea("ChartArea1") { Tag = "Tiempos de Espera" });
+            chartWaitingTimes.Series.Add("Tiempo Espera MM1");
+            chartWaitingTimes.Series.Add("Tiempo Espera MM2");
+            chartWaitingTimes.Series["Tiempo Espera MM1"].ChartType = SeriesChartType.Bar;
+            chartWaitingTimes.Series["Tiempo Espera MM2"].ChartType = SeriesChartType.Bar;
+            chartWaitingTimes.Series["Tiempo Espera MM1"].Color = System.Drawing.Color.Blue;
+            chartWaitingTimes.Series["Tiempo Espera MM2"].Color = System.Drawing.Color.Green;
+            chartWaitingTimes.ChartAreas["ChartArea1"].AxisX.Title = "Corrida";
+            chartWaitingTimes.ChartAreas["ChartArea1"].AxisY.Title = "Tiempo de Espera (min)";
+
+            // Ocultar lblConclusion inicialmente
+            lblConclusion.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Ejecutar_Click(object sender, EventArgs e)
+        {
+            // Leer parámetros desde los controles
+            NUM_RUNS = (int)nudSimulaciones.Value;
+            LAMBDA = (double)nudClientes.Value / 2.0; // Clientes cada 2 minutos -> tasa por minuto
+            MU = 1.0 / (double)nudDespacho.Value; // Tasa de servicio por minuto
+
+            // Validar entradas
+            if (LAMBDA <= 0 || MU <= 0 || NUM_RUNS <= 0)
+            {
+                MessageBox.Show("Por favor, ingrese valores válidos: tasas positivas y corridas > 0.");
+                return;
+            }
+
+            // Ejecutar simulación
+            EjecutarSimulacion();
+        }
+
+        // MÓDULO 1: Generación de números pseudoaleatorios
+        private List<double> GenerarNumerosPseudoaleatorios(int cantidad, long semilla)
+        {
+            List<double> numeros = new List<double>();
+            int X = (int)(semilla % m);
+            for (int i = 0; i < cantidad; i++)
+            {
+                X = (a * X + c) % m;
+                double Ri = (double)X / m;
+                numeros.Add(Ri);
+            }
+            return numeros;
+        }
+
+        // MÓDULO 2: Simulación de cola M/M/1
+        private (double avgWaitingTime, double avgServiceTime, double avgQueueLength) SimularColaMM1(List<double> serviceTimes, List<double> numeros, int startIdx, int endIdx)
+        {
+            double tiempoSimulacion = 0;
+            Queue<double> cola = new Queue<double>();
+            List<double> tiemposEspera = new List<double>();
+            double tiempoFinUltimoServicio = 0;
+            int idx = startIdx;
+            int clientesAtendidos = 0;
+            double totalQueueLength = 0;
+            int queueLengthSamples = 0;
+
+            while (tiempoSimulacion < SIMULATION_TIME && idx < endIdx)
+            {
+                double tiempoProximaLlegada = -Math.Log(1 - numeros[idx++]) / LAMBDA;
+                if (tiempoProximaLlegada + tiempoSimulacion <= tiempoFinUltimoServicio)
+                {
+                    cola.Enqueue(tiempoProximaLlegada + tiempoSimulacion);
+                    tiempoSimulacion += tiempoProximaLlegada;
+                    totalQueueLength += cola.Count;
+                    queueLengthSamples++;
+                }
+                else
+                {
+                    if (cola.Count > 0)
+                    {
+                        double tiempoLlegada = cola.Dequeue();
+                        double tiempoEspera = tiempoFinUltimoServicio - tiempoLlegada;
+                        tiemposEspera.Add(tiempoEspera);
+                        double tiempoServicio = -Math.Log(1 - numeros[idx++]) / MU;
+                        serviceTimes.Add(tiempoServicio);
+                        tiempoFinUltimoServicio += tiempoServicio;
+                        tiempoSimulacion = tiempoFinUltimoServicio;
+                        clientesAtendidos++;
+                        totalQueueLength += cola.Count;
+                        queueLengthSamples++;
+                    }
+                    else
+                    {
+                        tiempoSimulacion += tiempoProximaLlegada;
+                        tiempoFinUltimoServicio = tiempoSimulacion;
+                        double tiempoServicio = -Math.Log(1 - numeros[idx++]) / MU;
+                        serviceTimes.Add(tiempoServicio);
+                        tiempoFinUltimoServicio += tiempoServicio;
+                        clientesAtendidos++;
+                    }
+                }
+            }
+
+            double avgWaitingTime = tiemposEspera.Any() ? tiemposEspera.Average() : 0;
+            double avgServiceTime = serviceTimes.Any() ? serviceTimes.Average() : 0;
+            double avgQueueLength = queueLengthSamples > 0 ? totalQueueLength / queueLengthSamples : 0;
+            return (avgWaitingTime, avgServiceTime, avgQueueLength);
+        }
+
+        // MÓDULO 3: Simulación de cola M/M/2
+        private (double avgWaitingTime, double avgServiceTime, double avgQueueLength) SimularColaMM2(List<double> serviceTimes, List<double> numeros, int startIdx, int endIdx)
+        {
+            double tiempoSimulacion = 0;
+            Queue<double> cola = new Queue<double>();
+            List<double> tiemposEspera = new List<double>();
+            double[] tiemposFinServicio = { 0, 0 };
+            int servidoresOcupados = 0;
+            int idx = startIdx;
+            int clientesAtendidos = 0;
+            double totalQueueLength = 0;
+            int queueLengthSamples = 0;
+
+            while (tiempoSimulacion < SIMULATION_TIME && idx < endIdx)
+            {
+                double tiempoProximaLlegada = -Math.Log(1 - numeros[idx++]) / LAMBDA;
+                double minTiempoFinServicio = servidoresOcupados > 0 ? tiemposFinServicio.Min() : double.MaxValue;
+
+                if (tiempoProximaLlegada + tiempoSimulacion <= minTiempoFinServicio)
+                {
+                    // Procesar llegada
+                    tiempoSimulacion += tiempoProximaLlegada;
+                    cola.Enqueue(tiempoSimulacion);
+                    totalQueueLength += cola.Count;
+                    queueLengthSamples++;
+                }
+                else if (servidoresOcupados > 0)
+                {
+                    // Procesar fin de servicio
+                    tiempoSimulacion = minTiempoFinServicio;
+                    int servidor = Array.IndexOf(tiemposFinServicio, minTiempoFinServicio);
+                    tiemposFinServicio[servidor] = 0;
+                    servidoresOcupados--;
+                }
+                else
+                {
+                    // Avanzar al próximo evento de llegada
+                    tiempoSimulacion += tiempoProximaLlegada;
+                    cola.Enqueue(tiempoSimulacion);
+                    totalQueueLength += cola.Count;
+                    queueLengthSamples++;
+                }
+
+                // Asignar clientes a servidores disponibles
+                while (cola.Count > 0 && servidoresOcupados < 2)
+                {
+                    double tiempoLlegada = cola.Dequeue();
+                    double tiempoEspera = tiempoSimulacion - tiempoLlegada;
+                    tiemposEspera.Add(tiempoEspera);
+                    int servidor = tiemposFinServicio[0] <= tiemposFinServicio[1] ? 0 : 1;
+                    double tiempoServicio = -Math.Log(1 - numeros[idx++]) / MU;
+                    serviceTimes.Add(tiempoServicio);
+                    tiemposFinServicio[servidor] = tiempoSimulacion + tiempoServicio;
+                    servidoresOcupados++;
+                    clientesAtendidos++;
+                    totalQueueLength += cola.Count;
+                    queueLengthSamples++;
+                }
+            }
+
+            double avgWaitingTime = tiemposEspera.Any() ? tiemposEspera.Average() : 0;
+            double avgServiceTime = serviceTimes.Any() ? serviceTimes.Average() : 0;
+            double avgQueueLength = queueLengthSamples > 0 ? totalQueueLength / queueLengthSamples : 0;
+            return (avgWaitingTime, avgServiceTime, avgQueueLength);
+        }
+
+        // MÓDULO 4: Ejecución de la simulación y evaluación
+        private void EjecutarSimulacion()
+        {
+            dataGridViewResults.Rows.Clear();
+            chartWaitingTimes.Series["Tiempo Espera MM1"].Points.Clear();
+            chartWaitingTimes.Series["Tiempo Espera MM2"].Points.Clear();
+            averageWaitingTimesMM1.Clear();
+            averageServiceTimesMM1.Clear();
+            averageQueueLengthsMM1.Clear();
+            averageWaitingTimesMM2.Clear();
+            averageServiceTimesMM2.Clear();
+            averageQueueLengthsMM2.Clear();
+
+            // Usar la hora del sistema como semilla
+            long semilla = DateTime.Now.Ticks;
+
+            int cantidadNumeros = (int)(SIMULATION_TIME * LAMBDA * 2); // Aproximación de eventos
+            int extra = (int)(cantidadNumeros * 0.5); // 50% más
+            List<double> numerosMM1 = GenerarNumerosPseudoaleatorios(cantidadNumeros + extra, semilla);
+            List<double> numerosMM2 = GenerarNumerosPseudoaleatorios(cantidadNumeros + extra, semilla + 1);
+            List<double> serviceTimesMM1 = new List<double>();
+            List<double> serviceTimesMM2 = new List<double>();
+
+            // Agregar encabezado para M/M/1
+            dataGridViewResults.Rows.Add("-- 1 Caja--", "", "", "", "");
+            for (int i = 0; i < NUM_RUNS; i++)
+            {
+                int startIdx = i * (cantidadNumeros / NUM_RUNS);
+                int endIdx = (i + 1) * (cantidadNumeros / NUM_RUNS);
+                var (avgWaitingTime, avgServiceTime, avgQueueLength) = SimularColaMM1(serviceTimesMM1, numerosMM1, startIdx, endIdx);
+                averageWaitingTimesMM1.Add(avgWaitingTime);
+                averageServiceTimesMM1.Add(avgServiceTime);
+                averageQueueLengthsMM1.Add(avgQueueLength);
+                dataGridViewResults.Rows.Add(i + 1, avgWaitingTime, avgServiceTime, avgQueueLength, "M/M/1");
+                chartWaitingTimes.Series["Tiempo Espera MM1"].Points.AddXY(i + 1, avgWaitingTime);
+            }
+
+            // Agregar encabezado para M/M/2
+            dataGridViewResults.Rows.Add("--2 Cajas--", "", "", "", "");
+            for (int i = 0; i < NUM_RUNS; i++)
+            {
+                int startIdx = i * (cantidadNumeros / NUM_RUNS);
+                int endIdx = (i + 1) * (cantidadNumeros / NUM_RUNS);
+                var (avgWaitingTime, avgServiceTime, avgQueueLength) = SimularColaMM2(serviceTimesMM2, numerosMM2, startIdx, endIdx);
+                averageWaitingTimesMM2.Add(avgWaitingTime);
+                averageServiceTimesMM2.Add(avgServiceTime);
+                averageQueueLengthsMM2.Add(avgQueueLength);
+                dataGridViewResults.Rows.Add(i + 1, avgWaitingTime, avgServiceTime, avgQueueLength, "M/M/2");
+                chartWaitingTimes.Series["Tiempo Espera MM2"].Points.AddXY(i + 1, avgWaitingTime);
+            }
+
+            // Evaluar y mostrar conclusión
+            double promedioEsperaMM1 = averageWaitingTimesMM1.Average();
+            double promedioDespachoMM1 = averageServiceTimesMM1.Average();
+            double proporcionSuperan5MinMM1 = (averageWaitingTimesMM1.Count(t => t > 5) / (double)NUM_RUNS) * 100;
+            double diferenciaHistoricaMM1 = Math.Abs(promedioEsperaMM1 - 4.5);
+
+            double promedioEsperaMM2 = averageWaitingTimesMM2.Average();
+            double promedioDespachoMM2 = averageServiceTimesMM2.Average();
+            double proporcionSuperan5MinMM2 = (averageWaitingTimesMM2.Count(t => t > 5) / (double)NUM_RUNS) * 100;
+            double diferenciaHistoricaMM2 = Math.Abs(promedioEsperaMM2 - 4.5);
+
+            string conclusion = "• Resultados con 1 caja (M/M/1):\n";
+            conclusion += $"  - Tiempo promedio de espera: {promedioEsperaMM1:F2} minutos\n";
+            conclusion += $"  - Tiempo promedio de despacho: {promedioDespachoMM1:F2} minutos\n";
+            conclusion += $"  - Proporción de corridas con espera >5 min: {proporcionSuperan5MinMM1:F2}%\n";
+            conclusion += $"  - Diferencia con valor histórico (4.5 min): {diferenciaHistoricaMM1:F2} minutos";
+            conclusion += diferenciaHistoricaMM1 > 0.5 ? " (Diferencia significativa, revisar suposiciones)\n" : "\n";
+
+            conclusion += "• Resultados con 2 cajas (M/M/2):\n";
+            conclusion += $"  - Tiempo promedio de espera: {promedioEsperaMM2:F2} minutos\n";
+            conclusion += $"  - Tiempo promedio de despacho: {promedioDespachoMM2:F2} minutos\n";
+            conclusion += $"  - Proporción de corridas con espera >5 min: {proporcionSuperan5MinMM2:F2}%\n";
+            conclusion += $"  - Diferencia con valor histórico (4.5 min): {diferenciaHistoricaMM2:F2} minutos";
+            conclusion += diferenciaHistoricaMM2 > 0.5 ? " (Diferencia significativa, revisar suposiciones)\n" : "\n";
+
+            double reduccion = ((promedioEsperaMM1 - promedioEsperaMM2) / promedioEsperaMM1) * 100;
+            conclusion += "• Reducción del tiempo de espera con 2 cajas: " + reduccion.ToString("F2") + "%\n";
+            if (proporcionSuperan5MinMM1 > 50)
+            {
+                conclusion += "• Conclusión: " + (reduccion >= 50
+                    ? "Abrir una segunda caja cumple con el objetivo de reducción ≥ 50%."
+                    : "Abrir una segunda caja no cumple con el objetivo de reducción ≥ 50%.") + "\n";
+            }
+            else
+            {
+                conclusion += "• Conclusión: Mantener 1 caja es suficiente.\n";
+            }
+
+            // Aplicar formato de negrita a la conclusión
+            lblConclusion.Text = conclusion;
+            lblConclusion.Visible = true;
+            int boldStart = conclusion.IndexOf("Conclusión:");
+          
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            nudSimulaciones.Value = 10;
+            nudClientes.Value = 1;
+            nudDespacho.Value = 3;
+            dataGridViewResults.Rows.Clear();
+            chartWaitingTimes.Series["Tiempo Espera MM1"].Points.Clear();
+            chartWaitingTimes.Series["Tiempo Espera MM2"].Points.Clear();
+            lblConclusion.Visible = false;
+            averageWaitingTimesMM1.Clear();
+            averageServiceTimesMM1.Clear();
+            averageQueueLengthsMM1.Clear();
+            averageWaitingTimesMM2.Clear();
+            averageServiceTimesMM2.Clear();
+            averageQueueLengthsMM2.Clear();
         }
     }
 }
+
